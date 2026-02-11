da746de7a35314e0c39815f6204a8fd9a0a9c3bb
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2026-02-11 20:10:05 +0700

    use raycast instead of collision, spawn particles on a different container

--- Assets/Scripts/RayInteractor.cs
+++ Assets/Scripts/RayInteractor.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
+using UnityEngine.VFX;
 
-public class RayColorizer : MonoBehaviour
+public class RayInteractor : MonoBehaviour
 {
     // [SerializeField]
     // private GameObject Stick;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField]
+    private GameObject particlesContainer;
+
+    [SerializeField]
+    private GameObject particles;
+    
+    private GameObject lastCollided = null;
     void Start()
     {
         
@@ -18,11 +27,22 @@ public class RayColorizer : MonoBehaviour
         // Stick.transform.position = ray.origin;
         Debug.DrawRay(ray.origin, ray.direction, Color.coral);
 
-        if (Physics.Raycast(ray, out RaycastHit hitData, 200f, LayerMask.GetMask("Beats")))
+        if (Physics.Raycast(ray, out RaycastHit hitData, 2f, LayerMask.GetMask("Beats")))
         {
             GameObject hitObject = hitData.transform.gameObject;
+            if (lastCollided == hitObject)
+            {
+                return;
+            } 
+
             var renderer = hitObject.GetComponent<MeshRenderer>();
+
             renderer.material.color = Random.ColorHSV();
+
+            var particleInstance = Instantiate(particles, particlesContainer.transform, true);
+            particleInstance.transform.position = hitObject.transform.position;
+
+            lastCollided = hitObject;
         }
     }
 }
