45f4c5ef0eff7606227a4eb6dcb18f4f97573deb
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2026-03-21 00:02:17 +0700

    GOT THE FUCKING BUTTONS WORKINF

--- Assets/Scripts/UI/EndGameUI.cs
+++ Assets/Scripts/UI/EndGameUI.cs
@@ -3,10 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class EndGameUI : MonoBehaviour {
     public void GoToMenu() {
+        Debug.Log("Go to menu");
         SceneManager.LoadScene("UI", LoadSceneMode.Single);
     }
 
     public void RestartGame() {
+        Debug.Log("Restart Game");
         SceneManager.LoadScene("MainGame");
     }
 }
